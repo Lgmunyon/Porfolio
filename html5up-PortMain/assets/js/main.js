70d77ba542e7c38fdef814cb42ca3bb6dec4e362
Levi Munyon <lgmunyon@hotmail.com>
2023-02-21 16:59:37 -0500

    doom added

--- html5up-PortMain/assets/js/main.js
+++ html5up-PortMain/assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Read Only by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var $window = $(window),
@@ -157,3 +151,64 @@
 		});
 
 })(jQuery);
+
+
+/* --Keydown Event-- */
+
+// a key map of allowed keys
+var allowedKeys = {
+	37: 'left',
+	38: 'up',
+	39: 'right',
+	40: 'down',
+	65: 'a',
+	66: 'b'
+  };
+  
+  // the 'official' Konami Code sequence
+  var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+  
+  // a variable to remember the 'position' the user has reached so far.
+  var konamiCodePosition = 0;
+  
+  // add keydown event listener
+  document.addEventListener('keydown', function(e) {
+	// get the value of the key code from the key map
+	var key = allowedKeys[e.keyCode];
+	// get the value of the required key from the konami code
+	var requiredKey = konamiCode[konamiCodePosition];
+  
+	// compare the key with the required key
+	if (key == requiredKey) {
+  
+	  // move to the next key in the konami code sequence
+	  konamiCodePosition++;
+  
+	  // if the last key is reached, activate cheats
+	  if (konamiCodePosition == konamiCode.length) {
+		activateCheats();
+		konamiCodePosition = 0;
+	  }
+	} else {
+	  konamiCodePosition = 0;
+	}
+  });
+  
+  function activateCheats() {
+	let shown = document.getElementById("doom-container")
+	if (shown.style.display === "" || shown.style.display === "none"){
+		shown.style.display = "block"
+	}else {
+		shown.style.display === "none"
+	}
+	alert("cheats activated");
+  }
+
+  document.addEventListener('mouseup', function(e) {
+    var container = document.getElementById('doom-container');
+    if (!container.contains(e.target)) {
+        container.style.display = 'none';
+    }
+});
+
+ 
